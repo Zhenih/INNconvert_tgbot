c41439dce464a3bf54799c3a313d1ea5d8703f3f
zhenu <zhenu@DESKTOP-PQJUGPJ>
2024-05-16 14:35:41 +0300

    update: реализована обработка команды /help в соответствии с заданием, настроена клавиатура с кнопками всех команд

--- INNconvert_tgbot/Program.cs
+++ INNconvert_tgbot/Program.cs
@@ -1,12 +1,63 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Xml;
 using Telegram.Bot;
 using Telegram.Bot.Types;
+using Telegram.Bot.Types.ReplyMarkups;
 
 namespace INNconvert_tgbot
 {
     class Program
     {
+        private static async Task Start(ITelegramBotClient botClient, Message message)
+        {
+            var replyKeyboard = new ReplyKeyboardMarkup(
+                new List<KeyboardButton[]>()
+                {
+                                        new KeyboardButton[]
+                                        {
+                                            new KeyboardButton("/help"),
+                                            new KeyboardButton("/hello"),
+                                        },
+                                        new KeyboardButton[]
+                                        {
+                                            new KeyboardButton("/inn"),
+                                            new KeyboardButton("/last"),
+                                        }
+                })
+            {
+                ResizeKeyboard = true,
+            };
+
+            await botClient.SendTextMessageAsync(
+                message.Chat.Id,
+                "Выберите действие",
+                replyMarkup: replyKeyboard);
+        }
+
+        private static async Task Help(ITelegramBotClient botClient, Message message)
+        {
+            //сообщение выводимое при вызове /help
+            string help_message = "Спмсок доступных команд:\n/start – начать общение с ботом.\n/help – вывести справку о доступных командах.\n/hello – вывести имя и фамилию, email и ссылку на github автора бота.\n/inn – получить наименования и адреса компаний по ИНН.\n/last – повторить последнее действие бота.";
+            await botClient.SendTextMessageAsync(message.Chat.Id, help_message);
+        }
+
+        private static async Task Hello(ITelegramBotClient botClient, Message message)
+        {
+            await botClient.SendTextMessageAsync(message.Chat.Id, "/hello");
+        }
+
+        private static async Task Inn(ITelegramBotClient botClient, Message message)
+        {
+            await botClient.SendTextMessageAsync(message.Chat.Id, "/Inn");
+        }
+
+        private static async Task Last(ITelegramBotClient botClient, Message message)
+        {
+            await botClient.SendTextMessageAsync(message.Chat.Id, "/last");
+        }
+
+
         static void Main(string[] args)
         {
             //чтение ключа из xml
@@ -35,17 +86,42 @@ namespace INNconvert_tgbot
             throw new NotImplementedException();
         }
 
-        
+
         private static async Task Update(ITelegramBotClient botClient, Update update, CancellationToken token)
         {
+            
             var message = update.Message;
             if (message != null)
             {
                 if (message.Text != null)
                 {
-                    if (message.Text == "прив")
+                    switch (message.Text)
                     {
-                        await botClient.SendTextMessageAsync(message.Chat.Id, "хеллоу ворлд");
+                        case "/start":
+                            {
+                                Start(botClient, message);
+                                return;
+                            }
+                        case "/help":
+                            {
+                                Help(botClient, message);
+                                return;
+                            }
+                        case "/hello":
+                            {
+                                
+                                return;
+                            }
+                        case "/inn":
+                            {
+                                await botClient.SendTextMessageAsync(message.Chat.Id, "/inn");
+                                return;
+                            }
+                        case "/last":
+                            {
+                                await botClient.SendTextMessageAsync(message.Chat.Id, "/last");
+                                return;
+                            }
                     }
                 }
             }
